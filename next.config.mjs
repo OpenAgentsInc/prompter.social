f45283b1d52b78b76f9ab288fdd9b883ea653272
Christopher David <chris@arcadelabs.co>
2023-04-23 01:14:21 -0400

    ts ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
